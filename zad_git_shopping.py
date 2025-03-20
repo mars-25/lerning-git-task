c0d399b75e047eba5ebd4ba9ecca58ff2ffcd330
mars-25 <marswacht@gmail.com>
2025-03-20 14:04:32 +0100

    Działający program

--- zad_git_shopping.py
+++ zad_git_shopping.py
@@ -4,3 +4,11 @@ shopping = {
     'zoologiczny': ['python', 'myszy', 'motyle'],
 }
 
+licznik = 0
+for sklep, produkty in shopping.items():
+    print(f"Idę do {sklep.title()} i kupuję tem:")
+    for produkt in produkty:
+        print(f"\t{produkt.title()}")
+        licznik +=1
+
+print(f"\nW sumie kupuję {licznik} produktów.")
